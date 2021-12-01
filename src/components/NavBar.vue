a550815300aeca5cfc1f9ac291362cd757ec1596
Umut Elicabuk <amiculus.umut@hotmail.de>
2021-12-01 16:42:07 +0100

    add transition to nav items

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,9 +2,10 @@
   <b-navbar class="navbar" toggleable="lg" type="dark" variant="primary">
     <b-navbar-brand to="home">Umut</b-navbar-brand>
     <b-navbar-nav class="m-auto">
-      <b-nav-item id="nav-item" to="home">Home</b-nav-item>
-      <b-nav-item id="nav-item" to="home">Skillset</b-nav-item>
-      <b-nav-item id="nav-item" to="home">Photography</b-nav-item>
+      <b-nav-item class="nav-item" to="home">Home</b-nav-item>
+      <b-nav-item class="nav-item" to="home">Skillset</b-nav-item>
+      <b-nav-item class="nav-item" to="home">Photography</b-nav-item>
+      <b-nav-item class="nav-item" to="home">Contact</b-nav-item>
     </b-navbar-nav>
   </b-navbar>
 </template>
@@ -16,9 +17,25 @@ export default {
 </script>
 
 <style lang="scss">
-#nav-item {
+.nav-item {
     margin-left: 1rem;
     margin-right: 1rem;
     color: $text;
+    position: relative;
+}
+
+.nav-item::after {
+  content: '';
+  position: absolute;
+  bottom: 0%;
+  width: 0%;
+  height: 1px;
+  background: $text;
+  transition: width 300ms;
+}
+
+.nav-item:hover::after,
+.nav-item:focus::after {
+  width: 100%;
 }
 </style>
